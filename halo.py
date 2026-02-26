dac76ecb203536ee4dcfba4663adf87170baa794
Ahmadalzahra31 <ahmadalzahra31@icloud.com>
2026-02-26 08:35:12 -0500

    menambah file halo.py

--- halo.py
+++ halo.py
@@ -1,1 +1,1 @@
-print("Hallo Dunia!!! Saya adalah mantan opreker Python di OS Symbian Nokia dulu :)Saya nostalgia coding sekarang dengan OS Linux Antix")
+print("Halo Dunia!!! Saya adalah mantan opreker python di Symbian OS Nokia S60v Series dulu!!! sekarang saya nostalgia coding python di OS linux AntiX")
